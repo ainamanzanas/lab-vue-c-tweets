82f25ea973b8c7028ce8a330c91b49e8b3714286
Aina Manzanas <aina.manzanas@gmail.com>
2023-05-17 19:52:20 +0200

    done

---
+++ src/components/User.vue
@@ -0,0 +1,18 @@
+<template>
+    <span class="user">
+        <span class="name">{{user.name}}</span>
+        <span class="handle">@ {{user.handle}}</span>
+    </span>
+</template>
+
+<script setup>
+    import {defineProps} from 'vue'
+
+    defineProps({
+        user: Object
+        })
+</script>
+
+<style scoped>
+
+</style>
